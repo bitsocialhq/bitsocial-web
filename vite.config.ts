20e66eaadf1fc8cbf019c88c2473d3bdc205c83c
plebeius <tom@plebbit.com>
2026-01-31 14:52:25 +0800

    update package.json, add dev tools

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from "rolldown-vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import reactScan from "@react-scan/vite-plugin-react-scan";
+
+const isProduction = process.env.NODE_ENV === "production";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    !isProduction &&
+      reactScan(),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
